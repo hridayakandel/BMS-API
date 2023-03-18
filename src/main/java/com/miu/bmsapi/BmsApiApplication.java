40a52b8f5822067a2944d31fcc87b3b3cb41fa56
hridayakandel <hridayakandel@gmail.com>
2023-03-18 18:20:51 -0500

    initial setup

---
+++ src/main/java/com/miu/bmsapi/BmsApiApplication.java
@@ -0,0 +1,14 @@
+package com.miu.bmsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BmsApiApplication.class, args);
+    }
+
+}
+git
